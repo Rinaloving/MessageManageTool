c94a1d53f6b0915bdb9cfd6b4d46a86c1f740e63
cfl <1243455143@qq.com>
2018-12-24 11:03:17 +0800

    用lambda改写

--- MessageSelectTool/Form1.cs
+++ MessageSelectTool/Form1.cs
@@ -46,15 +46,14 @@ namespace MessageSelectTool
             //ArrayList<string> list = F
             string filePath = textBox1.Text.Trim()+"\\";
             List<string> filelist  = Directory.GetFiles(filePath,"*.xml").ToList();
-            foreach (var item in filelist)
-            {
+            filelist.ForEach(item=>{ 
                 string filename = Path.GetFileName(item);
-                textBox2.AppendText(filename+"\r\n");
+                textBox2.AppendText(filename + "\r\n");
                 string newPath = filename.Substring(3, 6); //371402 区号
-                CreateDirectory(filePath+ newPath);
-                string subPath = "20"+filename.Substring(9,2); //2018 年份
-                CreateDirectory(filePath + newPath+"\\"+subPath);
-                string rsubPath = filename.Substring(11,2); // 11 月份
+                CreateDirectory(filePath + newPath);
+                string subPath = "20" + filename.Substring(9, 2); //2018 年份
+                CreateDirectory(filePath + newPath + "\\" + subPath);
+                string rsubPath = filename.Substring(11, 2); // 11 月份
                 CreateDirectory(filePath + newPath + "\\" + subPath + "\\" + rsubPath);
                 string destFileName = filePath + newPath + "\\" + subPath + "\\" + rsubPath;
                 try
@@ -62,17 +61,16 @@ namespace MessageSelectTool
                     //File.Move(item, destFileName + "\\" + filename); // 移动报文到对应的文件夹下
                     //直接移动遇到已存在文件容易挂掉
                     //改为先复制，再删除的方式移动文件
-                    File.Copy(item, destFileName + "\\" + filename,true);
+                    File.Copy(item, destFileName + "\\" + filename, true);
                     File.Delete(item);
                 }
                 catch (Exception ex)
                 {
-                    MessageBox.Show("报文："+ filename +"移动失败！原因："+ex.Message);
+                    MessageBox.Show("报文：" + filename + "移动失败！原因：" + ex.Message);
                     throw;
                 }
-                
-
-            }
+            });
+            
         }
 
         public void CreateDirectory(string name)
@@ -80,14 +78,11 @@ namespace MessageSelectTool
             try
             {
                 if (!Directory.Exists(name))
-                {
                     Directory.CreateDirectory(name);
 
-                }
             }
             catch (Exception ex)
             {
-
                 MessageBox.Show("创建文件夹："+name+"异常！"+ex.Message);
                 throw;
             }
